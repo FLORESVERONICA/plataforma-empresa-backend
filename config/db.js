82b878499f82c438a80235d6a1495a8a1c88c336
Veronica_Flores <floresnateraveronica@gmail.com>
2024-12-05 13:40:49 +0100

    'mongolisto'

--- config/db.js
+++ config/db.js
@@ -5,10 +5,8 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
+
         console.log("MomgoDB conectado");
     
     } catch(err) {
